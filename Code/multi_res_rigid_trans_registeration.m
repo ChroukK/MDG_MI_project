49d4636bfd6bf9d76aff97cc362ef749d7294143
Chrouk Kasem <shorouq.kassem@mail.mcgill.ca>
2018-04-16 19:25:18 -0400

    added images

--- Code/multi_res_rigid_trans_registeration.m
+++ Code/multi_res_rigid_trans_registeration.m
@@ -22,7 +22,7 @@
 % [optimizer,metric] = imregconfig('multimodal');
 % optimizer.InitialRadius = 0.004;
 % 
-% prealigned_ct_img_001 = imregister(ct_img_001,ct_img_001_ref, mr_t1_img_001, mr_t1_img_001_ref, 'rigid', optimizer, metric);
+% %prealigned_ct_img_001 = imregister(ct_img_001,ct_img_001_ref, mr_t1_img_001, mr_t1_img_001_ref, 'rigid', optimizer, metric);
 % prealigned_mr_t1_img_001 = imregister(mr_t1_img_001, mr_t1_img_001_ref, ct_img_001,ct_img_001_ref,'rigid', optimizer, metric);
 %geomtform = imregtform(mr_t1_img_001, mr_t1_img_001_ref, ct_img_001,ct_img_001_ref, 'rigid', optimizer, metric);
 % figure
@@ -35,51 +35,51 @@
 % figure
 % imshow3D(prealigned_ct_img_001); 
 %% Perturb image
-perturbed_mr_t1_img_001 = imtranslate(prealigned_mr_t1_img_001, [0 0], 'FillValues', mode(prealigned_mr_t1_img_001(:)));
-%perturbed_mr_t1_img_001 = imrotate3(perturbed_mr_t1_img_001, 8, [1 0 0], 'crop', 'FillValues', mode(prealigned_mr_t1_img_001(:)));
-%perturbed_mr_t1_img_001 = imrotate3(perturbed_mr_t1_img_001, -16, [0 1 0], 'crop', 'FillValues', mode(prealigned_mr_t1_img_001(:)));
-perturbed_mr_t1_img_001 = imrotate3(perturbed_mr_t1_img_001, 16, [0 0 1], 'crop', 'FillValues', mode(prealigned_mr_t1_img_001(:)));
+perturbed_mr_t1_img_001 = imtranslate(prealigned_mr_t1_img_001, [-40 90], 'FillValues', mode(prealigned_mr_t1_img_001(:)));
+%perturbed_mr_t1_img_001 = imrotate3(perturbed_mr_t1_img_001, 16, [1 0 0], 'crop', 'FillValues', mode(prealigned_mr_t1_img_001(:)));
+%perturbed_mr_t1_img_001 = imrotate3(perturbed_mr_t1_img_001, 4, [0 1 0], 'crop', 'FillValues', mode(prealigned_mr_t1_img_001(:)));
+%perturbed_mr_t1_img_001 = imrotate3(perturbed_mr_t1_img_001, -8, [0 0 1], 'crop', 'FillValues', mode(prealigned_mr_t1_img_001(:)));
 % perturbed_z_disp_matrix = zeros(512, 512, 28, 3); 
 % perturbed_z_disp_matrix(:,:,:,3) = 2; 
 % translated_f_img = imwarp(perturbed_mr_t1_img_001, perturbed_z_disp_matrix, 'linear', 'FillValues', mode(perturbed_mr_t1_img_001(:)));
 figure
 imshow3D(perturbed_mr_t1_img_001); 
-
-f_img = perturbed_mr_t1_img_001(:,:,1:2:28); 
-r_img = ct_img_001(:,:,1:2:28);
-levels = 4; 
-
-r_x_region = 15; 
-x_trans_region = [1 -20]; 
-y_trans_region = [1 -20];
-z_trans_region = [1 -12];
-r_x_trans_region = [1 -22]; 
-r_y_trans_region = [1 -22]; 
-r_z_trans_region = [1 -22]; 
-S_x_trans_max_I = 0;
-S_y_trans_max_I = 0;
-S_z_trans_max_I = 0;
-S_x_trans_max_I_v = [];
-S_y_trans_max_I_v = [];
-S_z_trans_max_I_v = [];
-S_r_x_trans_max_I = 0;
-S_r_y_trans_max_I = 0;
-S_r_z_trans_max_I = 0;
-S_r_x_trans_max_I_v = [];
-S_r_y_trans_max_I_v = [];
-S_r_z_trans_max_I_v = [];
-
-for l=1:levels
-    if (l == 3)
-        break;
-    end
-    low_res_f_img = impyramid(f_img, 'reduce');
-    low_res_r_img = impyramid(r_img, 'reduce');
-    for i =1:levels-l
-        low_res_f_img = impyramid(low_res_f_img, 'reduce');
-        low_res_r_img = impyramid(low_res_r_img, 'reduce');
-    end
-    [r_img_signed_MDG_field r_img_mag_signed_MDG_field] = compute_MDG_vector_field(low_res_r_img);
+% 
+% f_img = perturbed_mr_t1_img_001(:,:,1:2:28); 
+% r_img = ct_img_001(:,:,1:2:28);
+% levels = 4; 
+% 
+% r_x_region = 15; 
+% x_trans_region = [1 -20]; 
+% y_trans_region = [1 -20];
+% z_trans_region = [1 -12];
+% r_x_trans_region = [1 -22]; 
+% r_y_trans_region = [1 -22]; 
+% r_z_trans_region = [1 -22]; 
+% S_x_trans_max_I = 0;
+% S_y_trans_max_I = 0;
+% S_z_trans_max_I = 0;
+% S_x_trans_max_I_v = [];
+% S_y_trans_max_I_v = [];
+% S_z_trans_max_I_v = [];
+% S_r_x_trans_max_I = 0;
+% S_r_y_trans_max_I = 0;
+% S_r_z_trans_max_I = 0;
+% S_r_x_trans_max_I_v = [];
+% S_r_y_trans_max_I_v = [];
+% S_r_z_trans_max_I_v = [];
+% 
+% for l=1:levels
+%     if (l == 3)
+%         break;
+%     end
+%     low_res_f_img = impyramid(f_img, 'reduce');
+%     low_res_r_img = impyramid(r_img, 'reduce');
+%     for i =1:levels-l
+%         low_res_f_img = impyramid(low_res_f_img, 'reduce');
+%         low_res_r_img = impyramid(low_res_r_img, 'reduce');
+%     end
+%     [r_img_signed_MDG_field r_img_mag_signed_MDG_field] = compute_MDG_vector_field(low_res_r_img);
 % %% Rotations on x-axis
 %     MI4d_r_x_trans = [];
 %     D_of_T_r_x_trans = [];
@@ -127,28 +127,28 @@ for l=1:levels
 %     save([pwd '\registration\z_rotat_registration_workspace']); 
 
 %% Rotations on z-axis
-    MI4d_r_z_trans = [];
-    D_of_T_r_z_trans = [];
-    MI_r_z_trans = []; 
-    S_r_z_tran = [];
-    for r_z = round((S_r_z_trans_max_I+r_z_trans_region(l) -1)+(r_z_trans_region(2)/2^(l-1))):round((S_r_z_trans_max_I+r_z_trans_region(l) -1)-(r_z_trans_region(2)/2^(l-1)))
-        rotated_f_img = imrotate3(low_res_f_img, r_z, [0 0 1], 'crop', 'FillValues', mode(low_res_f_img(:)));
-        [f_img_signed_MDG_field f_img_mag_signed_MDG_field] = compute_MDG_vector_field(rotated_f_img);
-        MI4d_r_z_trans = [MI4d_r_z_trans compute_4D_MI(rotated_f_img, f_img_mag_signed_MDG_field, low_res_r_img, r_img_mag_signed_MDG_field, 32)]; 
-        D_of_T_r_z_trans = [D_of_T_r_z_trans compute_angle_measure_D(r_img_signed_MDG_field, f_img_signed_MDG_field, r_img_mag_signed_MDG_field, f_img_mag_signed_MDG_field)]; 
-        MI_r_z_trans = [MI_r_z_trans mi(rotated_f_img, low_res_r_img)]; 
-        fprintf('Rotations on z-axis: %d at level :%d\n', r_z, l);
-    end
-    r_z_trans_region = [r_z_trans_region round((S_r_z_trans_max_I+r_z_trans_region(l) -1)+(r_z_trans_region(2)/2^(l-1)))];    
-    S_r_z_tran = MI4d_r_z_trans.*D_of_T_r_z_trans;
-    r_z_trans_fig = figure
-    plot(round((S_r_z_trans_max_I+r_z_trans_region(l) -1)+(r_z_trans_region(2)/2^(l-1))):round((S_r_z_trans_max_I+r_z_trans_region(l) -1)-(r_z_trans_region(2)/2^(l-1))), S_r_z_tran);
-    title(['Level ' num2str(l) ' for z-axis rotations'])
-    savefig(r_z_trans_fig, [pwd '/registration/level_' num2str(l) '_regis_z_rot']);
-    [S_z_rot_max S_r_z_trans_max_I] = max(S_r_z_tran);
-    S_r_z_trans_max_I_v = [S_r_z_trans_max_I_v S_r_z_trans_max_I];
-    save([pwd '\registration\z_rotat_registration_workspace']); 
-    
+%     MI4d_r_z_trans = [];
+%     D_of_T_r_z_trans = [];
+%     MI_r_z_trans = []; 
+%     S_r_z_tran = [];
+%     for r_z = round((S_r_z_trans_max_I+r_z_trans_region(l) -1)+(r_z_trans_region(2)/2^(l-1))):round((S_r_z_trans_max_I+r_z_trans_region(l) -1)-(r_z_trans_region(2)/2^(l-1)))
+%         rotated_f_img = imrotate3(low_res_f_img, r_z, [0 0 1], 'crop', 'FillValues', mode(low_res_f_img(:)));
+%         [f_img_signed_MDG_field f_img_mag_signed_MDG_field] = compute_MDG_vector_field(rotated_f_img);
+%         MI4d_r_z_trans = [MI4d_r_z_trans compute_4D_MI(rotated_f_img, f_img_mag_signed_MDG_field, low_res_r_img, r_img_mag_signed_MDG_field, 32)]; 
+%         D_of_T_r_z_trans = [D_of_T_r_z_trans compute_angle_measure_D(r_img_signed_MDG_field, f_img_signed_MDG_field, r_img_mag_signed_MDG_field, f_img_mag_signed_MDG_field)]; 
+%         MI_r_z_trans = [MI_r_z_trans mi(rotated_f_img, low_res_r_img)]; 
+%         fprintf('Rotations on z-axis: %d at level :%d\n', r_z, l);
+%     end
+%     r_z_trans_region = [r_z_trans_region round((S_r_z_trans_max_I+r_z_trans_region(l) -1)+(r_z_trans_region(2)/2^(l-1)))];    
+%     S_r_z_tran = MI4d_r_z_trans.*D_of_T_r_z_trans;
+%     r_z_trans_fig = figure
+%     plot(round((S_r_z_trans_max_I+r_z_trans_region(l) -1)+(r_z_trans_region(2)/2^(l-1))):round((S_r_z_trans_max_I+r_z_trans_region(l) -1)-(r_z_trans_region(2)/2^(l-1))), S_r_z_tran);
+%     title(['Level ' num2str(l) ' for z-axis rotations'])
+%     savefig(r_z_trans_fig, [pwd '/registration/level_' num2str(l) '_regis_z_rot']);
+%     [S_z_rot_max S_r_z_trans_max_I] = max(S_r_z_tran);
+%     S_r_z_trans_max_I_v = [S_r_z_trans_max_I_v S_r_z_trans_max_I];
+%     save([pwd '\registration\z_rotat_registration_workspace']); 
+%     
 % %% x-axis translations 
 %     MI4d_x_trans = [];
 %     D_of_T_x_trans = [];
@@ -223,4 +223,4 @@ for l=1:levels
 %     S_z_trans_max_I_v = [S_z_trans_max_I_v S_z_trans_max_I];
 %     save([pwd '\registration\trans_rotat_registration_workspace']); 
 %     
-end
+%end
